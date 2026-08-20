1fef4731dd86e58bd7e23f944681df42ec97a08f
imonbhuiya <imon.cse15@gmail.com>
2026-08-21 00:03:05 +0200

    Create and write README file for anagram solution

--- src/main/java/org/example/neetcode/arraysAndHashing/validAnagram/isAnagram.java
+++ src/main/java/org/example/neetcode/arraysAndHashing/validAnagram/isAnagram.java
@@ -1,4 +1,41 @@
-package org.example.neetcode.arraysAndHashing.validAnagram;
+class Solution {
+    public boolean isAnagram(String s, String t) {
 
-public class isAnagram {
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        java.util.HashMap<Character, Integer> charCounts = new java.util.HashMap<>();
+
+        for (int i = 0; i < s.length(); i++) {
+
+            char letter = s.charAt(i);
+
+            if (charCounts.containsKey(letter)) {
+                int oldCount = charCounts.get(letter);
+                int newCount = oldCount + 1;
+                charCounts.put(letter, newCount);
+            } else {
+                charCounts.put(letter, 1);
+            }
+
+            char letter2 = t.charAt(i);
+
+            if (charCounts.containsKey(letter2)) {
+                int oldCount = charCounts.get(letter2);
+                int newCount = oldCount - 1;
+                charCounts.put(letter2, newCount);
+            } else {
+                charCounts.put(letter2, -1);
+            }
+        }
+
+        for (int count : charCounts.values()) {
+            if (count != 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
 }
